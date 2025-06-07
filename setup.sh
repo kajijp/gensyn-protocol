412cdf38831a5f08a27b6e53ed581c455de99d86
kajijp <wakkajijp@gmail.com>
2025-06-07 14:39:53 +0700

    .

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $EUID -ne 0 ]]; then
-   echo "[!] This script must be run as root" 
+   echo "[!] This script must be run as root"
    exit 1
 fi
 
@@ -27,65 +27,78 @@ fi
 check_container
 
 install_dependencies() {
-	add-apt-repository ppa:deadsnakes/ppa -y
-	apt update
 
-	apt install python3.10 python3.10-venv python3.10-dev -y
+		apt install software-properties-common -y
+		add-apt-repository ppa:deadsnakes/ppa -y
+        apt update
 
-	update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
-	update-alternatives --set python3 /usr/bin/python3.10
+        apt install -y python3.10 python3.10-venv python3.10-dev
 
-	apt install -y screen curl iptables build-essential git wget lz4 jq make gcc nano automake autoconf tmux htop nvme-cli libgbm1 pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip
+        update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.10 1
+        update-alternatives --set python3 /usr/bin/python3.10
 
-	curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
-	apt install -y nodejs
-	npm install -g n
-	n 20.18.0
-	export PATH="/usr/local/bin:$PATH"
-	echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bashrc
+        apt install -y screen curl iptables build-essential git wget lz4 jq make gcc nano automake autoconf tmux htop nvme-cli libgbm1 pkg-config libssl-dev libleveldb-dev tar clang bsdmainutils ncdu unzip
 
-	npm install -g yarn
+        curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
+        apt install -y nodejs
+        npm install -g n
+        n 20.18.0
+        export PATH="/usr/local/bin:$PATH"
+        echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bashrc
 
-	git clone https://github.com/gensyn-ai/rl-swarm/
-	cd rl-swarm || { echo "[!] Failed to enter rl-swarm directory!"; exit 1; }
+        npm install -g yarn
 
-	python3.10 -m venv .venv
-	source .venv/bin/activate
+        git clone https://github.com/gensyn-ai/rl-swarm/
+        cd rl-swarm || { echo "[!] Failed to enter rl-swarm directory!"; exit 1; }
 
-	npm install -g n
-	n 20.18.0
-	hash -r
+        python3.10 -m venv .venv
+        source .venv/bin/activate
+
+        npm install -g n
+        n 20.18.0
+        hash -r
 }
 
 GPU_Setup() {
-    install_dependencies
+    
+		https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb
+		dpkg -i cuda-keyring_1.1-1_all.deb
+		apt-get update
+		apt-get -y install cuda-toolkit-12-8
+		
+		install_dependencies
 
-	sed -i 's/^max_steps: .*/max_steps: 20/' hivemind_exp/configs/mac/grpo-qwen-2.5-0.5b-deepseek-r1.yaml
-	sed -i 's/^bf16: .*/bf16: false/' hivemind_exp/configs/mac/grpo-qwen-2.5-0.5b-deepseek-r1.yaml
-	
-	if [[ -f ./run_rl_swarm.sh ]]; then
-		./run_rl_swarm.sh
-	else
-		echo "[!] run_rl_swarm.sh not found! Setup may have failed."
-	fi
+        sed -i 's/^max_steps: .*/max_steps: 20/' hivemind_exp/configs/mac/grpo-qwen-2.5-0.5b-deepseek-r1.yaml
+        sed -i 's/^bf16: .*/bf16: false/' hivemind_exp/configs/mac/grpo-qwen-2.5-0.5b-deepseek-r1.yaml
+
+        if [[ -f ./run_rl_swarm.sh ]]; then
+                ./run_rl_swarm.sh
+        else
+                echo "[!] run_rl_swarm.sh not found! Setup may have failed."
+        fi
 }
 
 CPU_Setup() {
-    
-    install_dependencies
 
-	sed -i 's/^max_steps: .*/max_steps: 5/' hivemind_exp/configs/mac/grpo-qwen-2.5-0.5b-deepseek-r1.yaml
-	sed -i 's/^bf16: .*/bf16: false/' hivemind_exp/configs/mac/grpo-qwen-2.5-0.5b-deepseek-r1.yaml
-
-	export PYTORCH_MPS_HIGH_WATERMARK_RATIO=0.0
+    if command -v nvidia-smi &> /dev/null && nvidia-smi -L | grep -q "GPU"; then
+        echo "[INFO] NVIDIA GPU terdeteksi. Beralih ke GPU_Setup..."
+        GPU_Setup
+        return
+    fi
 	
-	if [[ -f ./run_rl_swarm.sh ]]; then
-		./run_rl_swarm.sh
-	else
-		echo "[!] run_rl_swarm.sh not found! Setup may have failed."
-	fi
-}
+	install_dependencies
 
+        sed -i 's/^max_steps: .*/max_steps: 5/' hivemind_exp/configs/mac/grpo-qwen-2.5-0.5b-deepseek-r1.yaml
+        sed -i 's/^bf16: .*/bf16: false/' hivemind_exp/configs/mac/grpo-qwen-2.5-0.5b-deepseek-r1.yaml
+
+        export PYTORCH_MPS_HIGH_WATERMARK_RATIO=0.0
+
+        if [[ -f ./run_rl_swarm.sh ]]; then
+                ./run_rl_swarm.sh
+        else
+                echo "[!] run_rl_swarm.sh not found! Setup may have failed."
+        fi
+}
 
 
 
